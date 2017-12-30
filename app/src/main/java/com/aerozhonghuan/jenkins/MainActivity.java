9fc8b804f914ebef8ccfab00f17f2e66507e135a
liukai <liukai@aerozhonghuan.com>
2017-12-30 17:58:09 +0800

    配置多渠道打包机制

--- app/src/main/java/com/aerozhonghuan/jenkins/MainActivity.java
+++ app/src/main/java/com/aerozhonghuan/jenkins/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("Test", BuildConfig.SERVER_URL);
+        Log.d("Test", "ip:" + BuildConfig.SERVER_URL);
     }
 
 }
