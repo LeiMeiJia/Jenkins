b2c61d87bf1096f4129b83fcfce60ab34a61513f
liukai <liukai@aerozhonghuan.com>
2018-04-19 18:22:44 +0800

    code

--- app/src/main/java/com/aerozhonghuan/java/thread/MyTask.java
+++ app/src/main/java/com/aerozhonghuan/java/thread/MyTask.java
@@ -5,12 +5,15 @@ import android.util.Log;
 
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * 线程池需考虑资源共享问题
  * 1、使用synchronized后，相当于串行执行，降低了效率
  * 2、使用AtomicInteger可以保持数据唯一，且并发执行
- * 3、Semaphore可以控制某个共享资源可被同时访问的次数
+ * 3、Semaphore 信号量仅仅是对池资源进行监控，但不保证线程的安全
+ * 4、Lock锁、读写锁
  * <p>
  * ThreadLocal的作用是提供线程内的局部变量，这种变量在线程的生命周期内起作用
  * Created by Administrator on 2018/3/20.
@@ -18,8 +21,9 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class MyTask implements Runnable {
     private static final String TAG = MyTask.class.getSimpleName();
-    private AtomicInteger atomicInteger = new AtomicInteger(100);
+    private AtomicInteger atomicInteger = new AtomicInteger(100); // 原子类
     private Semaphore semaphore = new Semaphore(1); // 信号量 相当于串行执行
+    private Lock lock = new ReentrantLock();
     private int i = 100;
 
     @Override
@@ -47,9 +51,22 @@ public class MyTask implements Runnable {
             SystemClock.sleep(100);
             i--;
             Log.d(TAG, i + "：结束运行" + "，thread name is：" + Thread.currentThread().getName());
-            semaphore.release(); // 释放一把锁
         } catch (InterruptedException e) {
             e.printStackTrace();
+        } finally {
+            semaphore.release(); // 释放一把锁
+        }
+    }
+
+    private void method4() {
+        try {
+            lock.lock();
+            Log.d(TAG, i + "：开始运行" + "，thread name is：" + Thread.currentThread().getName());
+            SystemClock.sleep(100);
+            i--;
+            Log.d(TAG, i + "：结束运行" + "，thread name is：" + Thread.currentThread().getName());
+        } finally {
+            lock.unlock();
         }
     }
 
